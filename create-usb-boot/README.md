fa85014ddf4a49321ab48ab6fc1f5d38d90a2235
Thai Nguyen Xuan <nxthai@outlook.com>
2025-09-01 13:18:42 +0700

    script to create usb boot

---
+++ create-usb-boot/README.md
@@ -0,0 +1,96 @@
+# Install grub tools
+```sh
+sudo apt install -y grub2 grub-efi grub-pc dosfstools mtools
+```
+
+# Create partitions
+Use parted or fdisk to partition the USB drive. A typical setup includes:
+EFI System Partition (ESP): A small FAT32 partition (e.g., 512MB) for UEFI boot.
+BIOS Boot Partition: A small, unformatted partition (e.g., 2MB) for legacy BIOS boot.
+Root Partition: The main partition for the Debian system (e.g., ext4). 
+```sh
+parted /dev/sdX mklabel gpt
+parted /dev/sdX mkpart primary fat32 1MiB 65MiB
+parted /dev/sdX set 1 bios_grub on
+parted /dev/sdX mkpart primary 65MiB 129MiB
+parted /dev/sdX set 2 boot on
+parted /dev/sdX mkpart primary ext4 129MiB 100%
+```
+# Format partitions
+```sh
+mkfs.vfat -F32 /dev/sdX1
+# The BIOS boot partition doesn't need a filesystem
+mkfs.ext4 /dev/sdX3
+```
+# Mount sdX to directory /mnt/usb
+```sh
+mkdir -p /media/usb
+mount /dev/sdX1 /media/usb
+mkdir -p /media/usb/efi
+mount /dev/sdX2 /media/usb/efi
+```
+
+# Install grub efi
+```sh
+grub-install --target=x86_64-efi \
+    --root-directory=/media/usb \
+    --efi-directory=/media/usb/efi \
+    --boot-directory=/media/usb/boot \
+    --removable --no-floppy --no-nvram \
+    --bootloader-id=GRUB \
+    --recheck
+```
+
+# Install grub BIOS
+```sh
+grub-install --target=i386-pc \
+    --root-directory=/media/usb \
+    --boot-directory=/media/usb/boot \
+    --recheck \
+    /dev/sdX
+```
+# Support boot Windows (wimboot)
+Download wimboot from https://github.com/ipxe/wimboot.git
+cp wimboot to boot partitions
+```sh
+cp wimboot /media/usb/boot
+```
+
+# Create grub config
+```sh
+mkdir -p /media/usb/boot/grub
+cat <<EOF > /media/usb/boot/grub/grub.cfg
+set timeout=5
+set default=0
+menuentry "Boot from USB" {
+    insmod part_gpt
+    insmod ext2
+    set root=(hd0,gpt1)
+    linux /vmlinuz root=/dev/sdX1 ro quiet splash
+    initrd /initrd.img
+}
+menuentry "Boot from Hard Drive" {
+    insmod part_gpt
+    insmod ext2
+    set root=(hd0,gpt2)
+    linux /vmlinuz root=/dev/sdY1 ro quiet splash
+    initrd /initrd.img
+}
+
+menuentry "Windows 11" {
+    insmod part_gpt
+    insmod ntfs
+    set root=(hd0,gpt2)
+    set isofile=/os/windows/Win11_24H2_English_x64.iso
+    loopback loop (hd0,gpt3)$isofile
+    linux16 /vmlinuz root=/dev/sdY1 ro quiet splash
+    initrd /initrd.img
+}
+EOF
+```
+
+# Note
+## Fix bug out of memory when booting in efi mode with tpm is enabled
+```sh
+rmmod tpm
+``` 
--- create-usb-boot/run.sh
+++ create-usb-boot/run.sh
@@ -1,54 +1,97 @@
-# Install grub
-```sh
+# Environment
+MOUNT_POINT=/tmp/usb
+USB_DRIVE=/dev/sdX
+BIOS_PARTITION_ID=1
+EFI_PARTITION_ID=2
+DATA_PARTITION_ID=3
+
+# Install grub tools
 sudo apt install -y grub2 grub-efi grub-pc dosfstools mtools
-```
+
+# Create partitions
+parted ${USB_DRIVE} mklabel gpt
+parted ${USB_DRIVE} mkpart primary fat32 1MiB 65MiB
+parted ${USB_DRIVE} set ${BIOS_PARTITION_ID} bios_grub on
+parted ${USB_DRIVE} mkpart primary 65MiB 129MiB
+parted ${USB_DRIVE} ${EFI_PARTITION_ID} boot on
+parted ${USB_DRIVE} mkpart primary ext4 129MiB 100%
+
+# Format partitions
+# The BIOS boot partition doesn't need a filesystem
+# Format UEFI partition to fat32
+mkfs.vfat -F32 ${USB_DRIVE}${BIOS_PARTITION_ID}
+# Formtat data partition to ext4
+mkfs.ext4 ${USB_DRIVE}${DATA_PARTITION_ID}
+
 # Mount sdX to directory /mnt/usb
-```sh
-mkdir -p /media/usb
-mount /dev/sdX1 /media/usb
-mkdir -p /media/usb/efi
-mount /dev/sdX2 /media/usb/efi
-```
+# mount bios partition
+mkdir -p ${MOUNT_POINT}
+mount ${USB_DRIVE}${BIOS_PARTITION_ID} ${MOUNT_POINT}
+# mount EFI partition
+mkdir -p ${MOUNT_POINT}/efi
+mount ${USB_DRIVE}${EFI_PARTITION_ID} ${MOUNT_POINT}/efi
 
 # Install grub efi
-```sh
 grub-install --target=x86_64-efi \
-    --root-directory=/media/usb \
-    --efi-directory=/media/usb/efi \
-    --boot-directory=/media/usb/boot \
+    --root-directory=${MOUNT_POINT} \
+    --efi-directory=${MOUNT_POINT}/efi \
+    --boot-directory=${MOUNT_POINT}/boot \
     --removable --no-floppy --no-nvram \
     --bootloader-id=GRUB \
     --recheck
-```
 
-# Install grub efi
+# Install grub BIOS
 ```sh
 grub-install --target=i386-pc \
-    --root-directory=/media/usb \
-    --boot-directory=/media/usb/boot \
+    --root-directory=${MOUNT_POINT} \
+    --boot-directory=${MOUNT_POINT}/boot \
     --recheck \
-    /dev/sdX
+    ${USB_DRIVE}
 ```
+# Support boot Windows (wimboot)
+cp windows/wimboot ${MOUNT_POINT}/boot
 
 # Create grub config
-```sh
-mkdir -p /media/usb/boot/grub
-cat <<EOF > /media/usb/boot/grub/grub.cfg
+mkdir -p ${MOUNT_POINT}/boot/grub
+cat <<EOF > ${MOUNT_POINT}/boot/grub/grub.cfg
 set timeout=5
 set default=0
-menuentry "Boot from USB" {
+menuentry "Debian 12" {
     insmod part_gpt
     insmod ext2
     set root=(hd0,gpt1)
     linux /vmlinuz root=/dev/sdX1 ro quiet splash
     initrd /initrd.img
 }
-menuentry "Boot from Hard Drive" {
+menuentry "Ubuntu 24.04" {
     insmod part_gpt
     insmod ext2
     set root=(hd0,gpt2)
     linux /vmlinuz root=/dev/sdY1 ro quiet splash
     initrd /initrd.img
 }
+
+menuentry "Windows 11" {
+    insmod part_gpt
+    insmod ntfs
+    insmod fat
+    insmod loopback
+
+    set isofile=/os/windows/Win11_24H2_English_x64.iso
+    loopback loop (hd0,gpt3)$isofile
+
+    linux16 (hd0,gpt3)/boot/wimboot
+    initrd16 \
+        newc:bcd:(loop)/boot/bcd \
+        newc:boot.sdi:(loop)/boot/boot.sdi \
+        newc:boot.wim:(loop)/sources/boot.wim
+    boot
+}
 EOF
 ```
+
+# Note
+## Fix bug out of memory when booting in efi mode with tpm is enabled
+```sh
+rmmod tpm
+``` 
